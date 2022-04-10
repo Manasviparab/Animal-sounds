5b964ac0b362b4d89a6f322dd2dfb2665f81b220
Manasviparab <78352947+Manasviparab@users.noreply.github.com>
2022-04-10 11:16:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 //https://teachablemachine.withgoogle.com/models/LtaPFwiIq
+
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/LtaPFwiIq/model.json',modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
